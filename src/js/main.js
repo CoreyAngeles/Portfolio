322e09e0990368d790661953f517c2f2c979de37
Ilya_korikov <warrzone@yandex.ru>
2025-02-28 17:57:39 +0300

    add adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 AOS.init();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerButton = document.querySelector(".nav-menu__burger-btn");
+  const header = document.querySelector(".header");
+  const links = document.querySelectorAll(".nav-menu-list__link");
+  const screenWidth = window.screen.width;
+
+  const toggleOpenClass = () => {
+    header.classList.toggle("open");
+  };
+
+  burgerButton.addEventListener("click", toggleOpenClass);
+
+  links.forEach((link) => {
+    if (screenWidth <= 610) {
+      link.addEventListener("click", toggleOpenClass);
+    }
+  });
+});
